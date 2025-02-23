4e1915a403c04c410622b9310684ea6e773dc865
Frefdt <ferrier2705@gmail.com>
2025-02-23 19:58:40 +0000

    Moved Player to main assets instead of bank, as it will be used by multiple parts of program and updated bank components to use namespace instead. Removed placeholder player class

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,29 @@
-//Placeholder player class
+using System;
 
-public class Player{
-    
+namespace PropertyTycoon
+{
+    public class Player 
+    {
+        public string Name { get; set; }
+        public int Balance { get; set; }
+
+        public Player(string name)
+        {
+            // Set Player's Name and Handle null exception
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Balance = 1500; // Player starts with £1500
+        }
+
+        // Adding money to the Player's Account
+        public void Credit(int amount)
+        {
+            Balance += amount;
+        }
+
+        // Removing money from the Player's Account
+        public void Debit(int amount)
+        {
+            Balance -= amount;
+        }
+    }
 }

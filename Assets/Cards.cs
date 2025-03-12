3e9ce56c990ae6876f9c00933f5d34dd97dd517d
AniKazi001 <m.k.anik993@gmail.com>
2025-03-12 13:10:08 +0000

    added movement related cards
    jail related cards sill left to do

---
+++ Assets/Cards.cs
@@ -0,0 +1,218 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PropertyTycoon
+{
+    public class Card
+    {
+        public string Description { get; private set; }
+        public string Action { get; private set; }
+
+        public Card(string description, string action)
+        {
+            Description = description;
+            Action = action;
+        }
+    }
+
+    public static class Cards
+    {
+        // Referencing the PropertyManager class for movement related cards
+        private static PropertyManager propertyManager = GameObject.FindFirstObjectByType<PropertyManager>();
+        public static List<Card> PotLuck = new List<Card>()
+        {
+            new Card("You inherit £200", "Bank pays player £200"),
+            new Card("You have won 2nd prize in a beauty contest, collect £50", "Bank pays player £50"),
+            new Card("You are up the creek with no paddle - go back to the Old Creek", "Player token moves backwards to the Old Creek"), 
+            new Card("Student loan refund. Collect £20", "Bank pays player £20"),
+            new Card("Bank error in your favour. Collect £200", "Bank pays player £200"),
+            new Card("Pay bill for text books of £100", "Player pays £100 to the bank"),
+            new Card("Mega late night taxi bill pay £50", "Player pays £50 to the bank"),
+            new Card("Advance to GO", "Player moves forwards to GO"),
+            new Card("From sale of Bitcoin you get £50", "Bank pays player £50"),
+            new Card("Bitcoin assets fall - pay off Bitcoin shortfall", "Player pays £50 to the bank"),
+            new Card("Pay a £10 fine or take Opportunity Knocks", "If fine paid, player puts £10 on free parking"),
+            new Card("Pay insurance fee of £50", "Player puts £50 on free parking"),
+            new Card("Savings bond matures, collect £100", "Bank pays £100 to the player"),
+            new Card("Go to jail. Do not pass GO, do not collect £200", "As the card says"),
+            new Card("Received interest on shares of £25", "Bank pays player £25"),
+            new Card("It's your birthday. Collect £10 from each player", "Player receives £10 from each player"),
+            new Card("Get out of jail free", "Retained by the player until needed. No resale or trade value")
+        };
+
+        public static List<Card> OpportunityKnocks = new List<Card>()
+        {
+            new Card("Bank pays you a dividend of £50", "Bank pays player £50"),
+            new Card("You have won a lip sync battle. Collect £100", "Bank pays player £100"),
+            new Card("Advance to Turing Heights", "Player token moves forwards to Turing Heights"),
+            new Card("Advance to Han Xin Gardens. If you pass GO, collect £200", "Player moves token"),
+            new Card("Fined £15 for speeding", "Player puts £15 on free parking"),
+            new Card("Pay university fees of £150", "Player pays £150 to the bank"),
+            new Card("Take a trip to Hove station. If you pass GO, collect £200", "Player moves token"),
+            new Card("Loan matures, collect £150", "Bank pays £150 to the player"),
+            new Card("You are assessed for repairs, £40/house, £115/hotel", "Player pays money to the bank"),
+            new Card("Advance to GO", "Player moves token"),
+            new Card("You are assessed for repairs, £25/house, £100/hotel", "Player pays money to the bank"),
+            new Card("Go back 3 spaces", "Player moves token"),
+            new Card("Advance to Skywalker Drive. If you pass GO, collect £200", "Player moves token"),
+            new Card("Go to jail. Do not pass GO, do not collect £200", "As the card says"),
+            new Card("Drunk in charge of a hoverboard. Fine £30", "Player puts £30 on free parking"),
+            new Card("Get out of jail free", "Retained by the player until needed. No resale or trade value")
+        };
+
+        // Draw the top card from the deck
+        public static Card DrawTopCard(List<Card> cards)
+        {
+            Card topCard = cards[0];
+            cards.RemoveAt(0);
+            cards.Add(topCard); // Moves the card to the bottom of the deck
+            return topCard;
+        }
+
+        // Shuffle the deck of cards
+        public static void Shuffle(List<Card> cards)
+        {
+            for (int i = 0; i < cards.Count; i++)
+            {
+                Card temp = cards[i];
+                int randomIndex = Random.Range(i, cards.Count);
+                cards[i] = cards[randomIndex];
+                cards[randomIndex] = temp;
+            }
+        }
+
+        public static void ExecuteCardAction(Card card, Player player, Bank bank, List<Player> players)
+        {
+            Debug.Log($"{card.Description} - {card.Action}");
+            switch (card.Action)
+            {
+                // Bank to Player transactions
+                case "Bank pays player £200":
+                    bank.MakePayment(player, 200);
+                    break;
+                case "Bank pays player £150":
+                    bank.MakePayment(player, 150);
+                    break;
+                case "Bank pays player £100":
+                    bank.MakePayment(player, 100);
+                    break;
+                case "Bank pays player £50":
+                    bank.MakePayment(player, 50);
+                    break;
+                case "Bank pays player £25":
+                    bank.MakePayment(player, 25);
+                    break;
+                case "Bank pays player £20":
+                    bank.MakePayment(player, 20);
+                    break;
+
+                // Player to Bank transactions
+                case "Player pays £150 to the bank":
+                    player.Debit(150);
+                    bank.ReceivePayment(150);
+                    break;
+                case "Player pays £100 to the bank":
+                    player.Debit(100);
+                    bank.ReceivePayment(100);
+                    break;
+                case "Player pays £50 to the bank":
+                    player.Debit(50);
+                    bank.ReceivePayment(50);
+                    break;
+
+                // Player to Player transactions
+                case "Player receives £10 from each player":
+                    foreach (Player p in players)
+                    {
+                        if (p != player)
+                        {
+                            p.Debit(10);
+                            player.Credit(10);
+                        }
+                    }
+                    break;
+
+                // Free Parking Funds
+                case "If fine paid, player puts £10 on free parking":
+                    player.Debit(10);
+                    bank.AddToFreeParking(10);
+                    break;
+                case "Player puts £50 on free parking":
+                    player.Debit(50);
+                    bank.AddToFreeParking(50);
+                    break;
+                case "Player puts £15 on free parking":
+                    player.Debit(15);
+                    bank.AddToFreeParking(15);
+                    break;
+                case "Player puts £30 on free parking":
+                    player.Debit(30);
+                    bank.AddToFreeParking(30);
+                    break;
+
+                // Player movement related cards
+                case "Player moves forwards to GO":
+                    int stepsToGo = 40 - player.bPlayer.TileCount;
+                    player.bPlayer.Move(stepsToGo);
+                    break;
+
+                case "Player token moves forwards to Turing Heights":
+                    Property turingHeights = propertyManager.getTileProperty(40); // Tile number for Turing Heights 40
+                    if (turingHeights != null)
+                    {
+                        int stepsToTuringHeights = (turingHeights.tileno - player.bPlayer.TileCount + 40) % 40;
+                        player.bPlayer.Move(stepsToTuringHeights);
+                    }
+                    break;
+
+                case "Player moves token to Han Xin Gardens":
+                    Property hanXinGardens = propertyManager.getTileProperty(25); // Tile number for Han Xin Gardens 25
+                    if (hanXinGardens != null)
+                    {
+                        int stepsToHanXinGardens = (hanXinGardens.tileno - player.bPlayer.TileCount + 40) % 40;
+                        player.bPlayer.Move(stepsToHanXinGardens);
+                    }
+                    break;
+
+                case "Player moves token to Skywalker Drive":
+                    Property skywalkerDrive = propertyManager.getTileProperty(12); // Tile number for Skywalker Drive 12
+                    if (skywalkerDrive != null)
+                    {
+                        int stepsToSkywalkerDrive = (skywalkerDrive.tileno - player.bPlayer.TileCount + 40) % 40;
+                        player.bPlayer.Move(stepsToSkywalkerDrive);
+                    }
+                    break;
+
+                case "Player moves token to The Old Creek":
+                    Property oldCreek = propertyManager.getTileProperty(2); // Tile number for The Old Creek 2
+                    if (oldCreek != null)
+                    {
+                        int stepsToOldCreek = (oldCreek.tileno - player.bPlayer.TileCount + 40) % 40;
+                        player.bPlayer.Move(stepsToOldCreek);
+                    }
+                    break;
+
+                case "Player moves token backwards 3 spaces":
+                    player.bPlayer.TileCount -= 3;
+                    if (player.bPlayer.TileCount < 0)
+                        player.bPlayer.TileCount += 40;
+                    player.bPlayer.Move(-3);
+                    break;
+
+                /*case "Go to jail":
+                    Property jail = propertyManager.getTileProperty(11); // Assuming 11 is the tile for jail
+                    if (jail != null)
+                    {
+                        player.bPlayer.TileCount = jail.tileno;
+                        player.bPlayer.transform.position = *Jail tile position here* Vector3.zero; // Need Jil Position for this Card
+                   }
+                    break;*/
+
+
+                default:
+                    Debug.Log("Action not implemented");
+                    break;
+            }
+        }
+    }
+}
--- Assets/PropertyManager.cs
+++ Assets/PropertyManager.cs
@@ -11,28 +11,28 @@ namespace PropertyTycoon
         public void initialiseProperties()
         {
 
-            properties.Add(new Property("The Old Creek", 60, "Brown", 2));
-            properties.Add(new Property("Gangsters Paradise", 60, "Brown", 4));
-            properties.Add(new Property("The Angels Delight", 100, "Blue", 6));
-            properties.Add(new Property("Potters Avenue", 100, "Blue", 6));
-            properties.Add(new Property("Granger Drive", 120, "Blue", 8));
-            properties.Add(new Property("Skywalker Drive", 140, "Purple", 10));
-            properties.Add(new Property("Wookie Hole", 140, "Purple", 10));
-            properties.Add(new Property("Rey Lane", 160, "Purple", 12));
-            properties.Add(new Property("Bishop Drive", 180, "Orange", 14));
-            properties.Add(new Property("Dunham Street", 180, "Orange", 14));
-            properties.Add(new Property("Broyles Lane", 200, "Orange", 16));
-            properties.Add(new Property("Yue Fei Square", 220, "Red", 18));
-            properties.Add(new Property("Mulan Rouge", 220, "Red", 18));
-            properties.Add(new Property("Han Xin Gardens", 240, "Red", 20));
-            properties.Add(new Property("Shatner Close", 260, "Yellow", 22));
-            properties.Add(new Property("Picard Avenue", 260, "Yellow", 22));
-            properties.Add(new Property("Crusher Creek", 280, "Yellow", 22));
-            properties.Add(new Property("Sirat Mews", 300, "Green", 26));
-            properties.Add(new Property("Ghengis Crecent", 300, "Green", 26));
-            properties.Add(new Property("Ibis Close", 320, "Green", 28));
-            properties.Add(new Property("James Webb Way", 350, "DBlue", 35));
-            properties.Add(new Property("Turing Heights", 400, "DBlue", 50));
+            properties.Add(new Property("The Old Creek", 60, "Brown", 2));  //2
+            properties.Add(new Property("Gangsters Paradise", 60, "Brown", 4)); //4
+            properties.Add(new Property("The Angels Delight", 100, "Blue", 6)); //7
+            properties.Add(new Property("Potters Avenue", 100, "Blue", 6)); //9
+            properties.Add(new Property("Granger Drive", 120, "Blue", 8));  //10
+            properties.Add(new Property("Skywalker Drive", 140, "Purple", 10)); //12
+            properties.Add(new Property("Wookie Hole", 140, "Purple", 10)); //14
+            properties.Add(new Property("Rey Lane", 160, "Purple", 12));    //15
+            properties.Add(new Property("Bishop Drive", 180, "Orange", 14));    //17
+            properties.Add(new Property("Dunham Street", 180, "Orange", 14));   //19
+            properties.Add(new Property("Broyles Lane", 200, "Orange", 16));    //20
+            properties.Add(new Property("Yue Fei Square", 220, "Red", 18));   //22
+            properties.Add(new Property("Mulan Rouge", 220, "Red", 18));    //24
+            properties.Add(new Property("Han Xin Gardens", 240, "Red", 20));    //25    
+            properties.Add(new Property("Shatner Close", 260, "Yellow", 22));   //27
+            properties.Add(new Property("Picard Avenue", 260, "Yellow", 22));   //28
+            properties.Add(new Property("Crusher Creek", 280, "Yellow", 22));   //30
+            properties.Add(new Property("Sirat Mews", 300, "Green", 26));   //32
+            properties.Add(new Property("Ghengis Crecent", 300, "Green", 26));  //33
+            properties.Add(new Property("Ibis Close", 320, "Green", 28));   //35
+            properties.Add(new Property("James Webb Way", 350, "DBlue", 35));   //38
+            properties.Add(new Property("Turing Heights", 400, "DBlue", 50));   //40
 
             //this is a hacky fix but i'm really lazy
             int[] tileList = {2,4,7,9,10,12,14,15,17,19,20,22,24,25,27,28,30,32,33,35,38,40};

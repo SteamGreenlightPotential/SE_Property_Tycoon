e17dbc2ddf6286fcce920550b8a29699f4d14039
AniKazi001 <m.k.anik993@gmail.com>
2025-04-03 14:37:24 +0100

    some player count changes... and basic script

---
+++ Assets/Scripts/PlayerSelection.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI; // For using Dropdown
+
+public class PlayerSelection : MonoBehaviour
+{
+    public Dropdown playerDropdown; // Reference to the Dropdown
+    public static int numberOfPlayers; // Store the selected number of players
+
+    void Start()
+    {
+        // Set a default value, e.g., 2 players
+        numberOfPlayers = 2;
+
+        // Add listener to detect when dropdown value changes
+        playerDropdown.onValueChanged.AddListener(DropdownValueChanged);
+    }
+
+    void DropdownValueChanged(int value)
+    {
+        numberOfPlayers = value + 2; // Dropdown index starts from 0, so +2 for players
+        Debug.Log("Number of Players: " + numberOfPlayers);
+    }
+}
